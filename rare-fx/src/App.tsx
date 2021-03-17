820c87655c202a98d55b14e78714ed2b4abcd876
Samson303 <samson.mulugeta@code.berlin>
2021-03-17 13:35:21 +0000

    feature: foramtt setup

--- rare-fx/src/App.tsx
+++ rare-fx/src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
 function App() {
-  return <div className="App">RAREFX APP</div>;
+  return <div className="App">RAREFX APP</div>
 }
 
-export default App;
+export default App
