1a4df692be9df86c4788480e87748282b0d47ec8
Samson303 <samson.mulugeta@code.berlin>
2021-03-18 14:13:37 +0000

    feature: SliderProvider

---
+++ rare-fx/src/context/SliderProvider.tsx
@@ -0,0 +1,51 @@
+import React, {
+  createContext,
+  ReactElement,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+
+interface SliderProviderValue {
+  currentIndex: number
+  next: () => void
+  prev: () => void
+}
+
+const SliderContext = createContext({} as SliderProviderValue)
+
+function SliderProvider({ children }: { children: ReactNode }): ReactElement {
+  const [currentIndex, setIndex] = useState(0)
+
+  const numberOfIndexes = (numberOfItems: number) => {
+    return numberOfItems - 1
+  }
+  const next = (numberOfItems: number) => {
+    const numberOfIndex = numberOfIndexes(numberOfItems)
+    const newIndex = currentIndex === numberOfIndex ? 0 : currentIndex + 1
+    setIndex(newIndex)
+  }
+
+  const prev = (numberOfItems: number) => {
+    const numberOfIndex = numberOfIndexes(numberOfItems)
+    const newIndex = currentIndex === 0 ? numberOfIndex : currentIndex - 1
+  }
+
+  return (
+    <SliderContext.Provider
+      value={
+        {
+          currentIndex,
+          next,
+          prev,
+        } as SliderProviderValue
+      }
+    >
+      {children}
+    </SliderContext.Provider>
+  )
+}
+
+const useSlider = (): SliderProviderValue => useContext(SliderContext)
+export { SliderProvider, useSlider, SliderContext }
