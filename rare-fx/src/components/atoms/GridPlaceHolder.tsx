4816a9dd46ec05daa32853a05815ed1e3d658ebd
Samson303 <samson.mulugeta@code.berlin>
2021-03-18 10:14:30 +0000

    feature: show on HomePage

--- rare-fx/src/components/atoms/GridPlaceHolder.tsx
+++ rare-fx/src/components/atoms/GridPlaceHolder.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import Carousel from '../molecules/Carousel/Carousel'
 import styles from './Gridplaceholder.module.scss'
 
 const GridPlaceHolder: React.FC = () => {
-    return(
-        <div className={styles.placeholder}>
-            <div className={styles.verticalCenter}> Grid Placeholder</div>
-        </div>
-    )
+  const images = [
+    'https://picsum.photos/400/600',
+    'https://picsum.photos/400/600',
+    'https://picsum.photos/400/600',
+  ]
+  return (
+    <div className={styles.placeholder}>
+      <div className={styles.verticalCenter}></div>
+      <Carousel imagesUrls={images} />
+    </div>
+  )
 }
 
 export default GridPlaceHolder
