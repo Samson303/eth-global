a981a4d4fab7c68bc371d94bb600c1d7e835627c
Samson303 <samson.mulugeta@code.berlin>
2021-03-19 18:28:17 +0000

    feature: cards connect to nvm

--- rare-fx/src/components/molecules/AlsoOnCard/AlsoOnCard.tsx
+++ rare-fx/src/components/molecules/AlsoOnCard/AlsoOnCard.tsx
@@ -2,25 +2,26 @@ import { url } from 'node:inspector'
 import React from 'react'
 import styles from './AlsoOnCard.module.scss'
 
-
 interface ICardProps {
-    imgUrl: string;
-    artworkTitle: string; 
+  serviceUrl?: string
+  serviceImageUrl?: string
+  serviceName?: string
+  nftName?: string
 }
 
-const AlsoOnCard: React.FC<ICardProps> = ({imgUrl, artworkTitle}) => {
-    return(
-        <div className={styles.cardWrapper}>
-            <div className={styles.image} style={{ backgroundImage: `url(${imgUrl})`}}></div>
-            <div className={styles.text}>
-                View {artworkTitle} on CV
-            </div>
-            <div className={styles.moreInfo}>
-                More Info?
-            </div>
-        </div>
-    )
+const AlsoOnCard: React.FC<ICardProps> = ({ serviceUrl, serviceImageUrl, serviceName }) => {
+  return (
+    <div className={styles.cardWrapper}>
+      <div
+        className={styles.image}
+        style={{ backgroundImage: `url(${serviceImageUrl})` }}
+      ></div>
+      <a href={serviceUrl}>
+        <div className={styles.text}>View on {serviceName}</div>
+      </a>
+      <div className={styles.moreInfo}>More Info?</div>
+    </div>
+  )
 }
-
 
 export default AlsoOnCard
--- rare-fx/src/components/molecules/AlsoOnDock/AlsoOnDock.tsx
+++ rare-fx/src/components/molecules/AlsoOnDock/AlsoOnDock.tsx
@@ -1,49 +1,82 @@
-import React, { useState } from 'react'
-import Docker from 'react-dock'
+import React, { useEffect, useState } from 'react'
 import { useDocker } from '../../../context/DockProvider'
-import Button from '../../atoms/Button'
-import DockItem from './DockItem'
 import styles from './AlsoOnDock.module.scss'
-import FadedRoundButton from '../../atoms/FadedRoundButton'
 import Emoji from '../../atoms/Emoji'
 import DockButton from '../../atoms/DockButton'
 import AlsoOnCard from '../AlsoOnCard/AlsoOnCard'
+import {
+  cryptoVoxelsImage,
+  cryptoVoxelsUrl,
+  openSeaImage,
+  openSeaUrl,
+  rareEffectImage,
+  rareEffectUrl,
+} from '../../atoms/ImageUrls'
+import { useSlider } from '../../../context/SliderProvider'
+import { useNvm } from '../../../context/NvmProvider'
 
-
-
-interface IProps {
-  cryptoVoxelUrl?: string
-  rareEffectUrl?: string
-  othersUrls?: string[]
-}
-
-const AlsoOnDock = ({ cryptoVoxelUrl, rareEffectUrl, othersUrls }: IProps) => {
-  const { isActive, toggleActive } = useDocker()
-
+const AlsoOnDock = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
+  const { currentIndex } = useSlider()
+  const { DDOs } = useNvm()
+  const [nftData, setNftData] = useState({
+    nftName: '',
+    nftAuthor: '',
+  })
+  const currentDDO = DDOs[currentIndex]
 
+  console.log(currentDDO)
   const handleToggle = () => {
     setIsOpen(!isOpen)
   }
 
+  useEffect(() => {
+    if (currentDDO)
+      setNftData({
+        nftName: currentDDO.service[0].attributes.main.name,
+        nftAuthor: currentDDO.service[0].attributes.main.author,
+      })
+  }, [currentIndex, DDOs])
+
   return (
     <>
-    {!isOpen && <DockButton onClick={handleToggle}><Emoji emoji={"🔮"} label={"dislike"}/></DockButton>}
-    {isOpen && 
-      <div className={styles.sticky}>
-        <div className={styles.cardWrapper}>
-          <button onClick={handleToggle} className={styles.closeButton}>
-            x
-          </button>
-          <div className={styles.cardGrid}>
-            <AlsoOnCard imgUrl={"https://via.placeholder.com/150"} artworkTitle={"The Future"}/>
-            <AlsoOnCard imgUrl={"https://via.placeholder.com/150"} artworkTitle={"The Future"}/>
-            <AlsoOnCard imgUrl={"https://via.placeholder.com/150"} artworkTitle={"The Future"}/>
-            <AlsoOnCard imgUrl={"https://via.placeholder.com/150"} artworkTitle={"The Future"}/>
+      {!isOpen && (
+        <DockButton onClick={handleToggle}>
+          <Emoji emoji={'🔮'} label={'dislike'} />
+        </DockButton>
+      )}
+      {isOpen && (
+        <div className={styles.sticky}>
+          <div className={styles.cardWrapper}>
+            {/* <div className={styles.artistName}>
+              <h3>
+                {nftData.nftName} by {nftData.nftAuthor}
+              </h3>
+            </div> */}
+            <button onClick={handleToggle} className={styles.closeButton}>
+              x
+            </button>
+            <div className={styles.cardGrid}>
+              <AlsoOnCard
+                serviceUrl={rareEffectUrl}
+                serviceImageUrl={rareEffectImage}
+                serviceName={'RareEffectV2'}
+              />
+              <AlsoOnCard
+                serviceUrl={cryptoVoxelsUrl}
+                serviceImageUrl={cryptoVoxelsImage}
+                serviceName={'VR'}
+              />
+              <AlsoOnCard
+                serviceUrl={openSeaUrl}
+                serviceImageUrl={openSeaImage}
+                serviceName={'OpenSea'}
+              />
+              <AlsoOnCard />
+            </div>
           </div>
         </div>
-      </div>
-      }
+      )}
     </>
   )
 }
