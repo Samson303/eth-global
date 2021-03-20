cc49d2f277b20be9c199acf6f343bc5da9562c69
Samson303 <samson.mulugeta@code.berlin>
2021-03-20 16:42:13 +0000

    feature: connect nfts

--- rare-fx/src/components/molecules/AlsoOnDock/AlsoOnDock.tsx
+++ rare-fx/src/components/molecules/AlsoOnDock/AlsoOnDock.tsx
@@ -6,40 +6,65 @@ import DockButton from '../../atoms/DockButton'
 import AlsoOnCard from '../AlsoOnCard/AlsoOnCard'
 import {
   cryptoVoxelsImage,
-  cryptoVoxelsUrl,
+  cryptoVoxelsGenericUrl,
   openSeaImage,
   openSeaUrl,
   rareEffectImage,
-  rareEffectUrl,
+  rareEffectGenericUrl,
 } from '../../atoms/ImageUrls'
 
 import { SlideDown } from 'react-slidedown'
 import 'react-slidedown/lib/slidedown.css'
 import { useNvm } from '../../../context/NvmProvider'
 import { useSlider } from '../../../context/SliderProvider'
+import { nftLocationData, NftLocationData } from '../../../data/NftLocationData'
 
-interface IProps {
-  cryptoVoxelUrl?: string
-  rareEffectUrl?: string
-  othersUrls?: string[]
-}
-
-const AlsoOnDock = ({ cryptoVoxelUrl, rareEffectUrl, othersUrls }: IProps) => {
+const AlsoOnDock = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const { currentIndex } = useSlider()
   const { DDOs } = useNvm()
   const [nftData, setNftData] = useState({
     nftName: '',
     nftAuthor: '',
+    cryptoVoxelsUrl: 'https://www.cryptovoxels.com/play?coords=N@335W,136N',
+    rareEffectUrl: '',
   })
   const currentDDO = DDOs[currentIndex]
 
+  const getNftCvLink = (nftname: string) => {
+    const result = nftLocationData.filter((data) => data.nftName === nftname)
+    if (result.length === 0) {
+      return cryptoVoxelsGenericUrl
+    }
+    const link = result[0]['cvLocation']
+    if (link) return link
+    return cryptoVoxelsGenericUrl
+  }
+
+  const getNftRareEffectLink = (nftname: string) => {
+    const result = nftLocationData.filter((data) => data.nftName === nftname)
+    if (result.length === 0) {
+      return rareEffectGenericUrl
+    }
+    const link = result[0]['rareEffectLink']
+    if (link) return link
+    return rareEffectGenericUrl
+  }
+
   useEffect(() => {
-    if (currentDDO)
+    if (currentDDO) {
+      const nftName = currentDDO.service[0].attributes.main.name
+      const nftAuthor = currentDDO.service[0].attributes.main.author
+      let cvLocation = getNftCvLink(nftName)
+      let rareEffectLink = getNftRareEffectLink(nftName)
+
       setNftData({
-        nftName: currentDDO.service[0].attributes.main.name,
-        nftAuthor: currentDDO.service[0].attributes.main.author,
+        nftName: nftName,
+        nftAuthor: nftAuthor,
+        cryptoVoxelsUrl: cvLocation,
+        rareEffectUrl: rareEffectLink,
       })
+    }
   }, [currentIndex, DDOs])
 
   const handleToggle = () => {
@@ -63,12 +88,12 @@ const AlsoOnDock = ({ cryptoVoxelUrl, rareEffectUrl, othersUrls }: IProps) => {
               </h3>
               <div className={styles.cardGrid}>
                 <AlsoOnCard
-                  serviceUrl={rareEffectUrl}
+                  serviceUrl={nftData.rareEffectUrl}
                   serviceImageUrl={rareEffectImage}
                   serviceName={'RareEffectV2'}
                 />
                 <AlsoOnCard
-                  serviceUrl={cryptoVoxelsUrl}
+                  serviceUrl={nftData.cryptoVoxelsUrl}
                   serviceImageUrl={cryptoVoxelsImage}
                   serviceName={'VR'}
                 />
